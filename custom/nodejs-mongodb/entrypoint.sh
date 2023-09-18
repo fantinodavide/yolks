18c2499d91b96e9c38179d665a342abf52184e2a
Fantino Davide <fantinodavide02@gmail.com>
2023-09-19 01:17:23 +0200

    chore: Update MongoDB configuration in entrypoint.sh

--- custom/nodejs-mongodb/entrypoint.sh
+++ custom/nodejs-mongodb/entrypoint.sh
@@ -5,9 +5,10 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
+# MongoDB
 [[ ! -e /home/container/mongodb_data ]] && mkdir /home/container/mongodb_data
-# Start MongoDB bound to 127.0.0.1
-mongod --bind_ip 127.0.0.1 --port ${MONGODB_INTERNAL_PORT} --dbpath /home/container/mongodb_data &
+[[ $MONGODB_QUIET -eq 1 ]] && QUIET_OPT="--quiet" || QUIET_OPT=""
+mongod --bind_ip 127.0.0.1 --port ${MONGODB_INTERNAL_PORT} --dbpath /home/container/mongodb_data $QUIET_OPT &
 
 # Print Node.js Version
 node -v
