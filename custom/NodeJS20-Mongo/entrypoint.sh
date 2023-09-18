bb9319e7c5f4276ab36ccc2217347487988435b6
Fantino Davide <fantinodavide02@gmail.com>
2023-09-18 23:08:25 +0200

    feat: nodejs + mongodb single yolk

---
+++ custom/NodeJS20-Mongo/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Start MongoDB bound to 127.0.0.1
+mongod --bind_ip 127.0.0.1 &
+
+# Print Node.js Version
+node -v
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
